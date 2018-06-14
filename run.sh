8c7807b476d74b53be3ea2f6d32c395b0c94fc81
Bernhard Bermeitinger <bernhard.bermeitinger@gmail.com>
2018-06-14 17:03:07 +0200

    merging two different sources

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 IMAGE=bermeitingerb/fedora-latex:latest
 
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/src "$IMAGE" "$@"
+exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
