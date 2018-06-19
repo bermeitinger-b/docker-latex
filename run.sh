a9c3c01a5513e67e7acbfe3b863b4aa9f6257fd9
Bernhard Bermeitinger <bernhard.bermeitinger@uni-passau.de>
2018-06-19 16:12:39 +0200

    Restrict memory consumption to at max 1GB

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 IMAGE=bermeitingerb/fedora-latex:latest
 
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
+exec docker run \
+	--memory 1g \
+	--memory-swap 1g \
+	--rm \
+	-i \
+	--user="$(id -u):$(id -g)" \
+	--net=none \
+	-v "$PWD":/data \
+	"$IMAGE" \
+	"$@"
