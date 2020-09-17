8b5463c2d28eab13dd482d330cc2155106d80b2d
Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>
2020-09-17 13:19:22 +0200

    Print output when parallel building

--- parallel_build.sh
+++ parallel_build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
 # uses parallel to build TeXLive 2016, 2017, 2018, 2019, and 2020
-parallel --jobs 8 --line-buffer ./build.sh {} ::: 2016 2017 2018 2019 2020
+parallel \
+	--ungroup \
+    ./build.sh {} ::: 2016 2017 2018 2019 2020
